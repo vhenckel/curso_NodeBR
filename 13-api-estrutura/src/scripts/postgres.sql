b0374cebfa00782a955d4248a38dedb37adcabab
Vitor Henckel <vhenckel627@hotmail.com>
2019-01-15 10:55:00 -0200

    Trabalhando com HapiJS

---
+++ 13-api-estrutura/src/scripts/postgres.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS TB_HEROIS;
+
+CREATE TABLE TB_HEROIS (
+    ID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    NOME TEXT NOT NULL,
+    PODER TEXT NOT NULL
+)
+
+INSERT INTO TB_HEROIS (NOME, PODER)
+VALUES 
+    ('Flash', 'Velocidade'),
+    ('Aquaman', 'Falar com os animais'),
+    ('Batman', 'Dinheiro');
+
+SELECT * FROM TB_HEROIS;

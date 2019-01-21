68ed16702a80f582d291887a76d38e860d50c551
Vitor Henckel <vhenckel627@hotmail.com>
2019-01-21 10:55:17 -0200

    Adicionado método de validação com Joi

---
+++ 15-hapi-listar-joi/src/tests/apiHerois.test.js
@@ -0,0 +1,49 @@
+const assert = require('assert')
+const api = require('./../api')
+let app = {}
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Chapolin Colorado',
+    poder: 'Marreta Biônica'
+}
+
+describe.only('Suite de testes da API Heroes', function () {
+    this.beforeAll(async () => {
+        app = await api
+    })
+
+    it('Listar /herois', async () => {
+        const result = await app.inject({
+            method: 'GET',
+            url: '/herois'
+        })
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        assert.deepEqual(statusCode, 200)
+        assert.ok(Array.isArray(dados))
+    })
+
+    it('Listar /herois - deve retornar somente x registros', async () => {
+        const TAMANHO_LIMITE = 3
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        })
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        assert.deepEqual(statusCode, 200)
+        assert.ok(dados.length === TAMANHO_LIMITE)
+    })
+
+    it('Listar /herois - deve filtrar somente 1 item', async () => {
+        const TAMANHO_LIMITE = 1000
+        const NAME = 'Homem Aranha-1546956227126'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}&nome=${NAME}`
+        })
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        assert.deepEqual(statusCode, 200)
+        assert.ok(dados[0].nome === NAME)
+    })
+})
